1b4a093ce6ee18b940ee7d04cef5bc9414d24dc2
Lucien <root@DESKTOP-7CO9QQK>
2024-08-03 13:53:00 +0200

    cleaner vec

--- srcs/PmergeMe.cpp
+++ srcs/PmergeMe.cpp
@@ -4,7 +4,9 @@
 PmergeMe::PmergeMe() :_deep(0) {}
 
 PmergeMe::~PmergeMe() {
-
+    for (std::vector<void *>::iterator it = _vec.begin(); it != _vec.end(); ++it) {
+        delete static_cast<unsigned long int *>(*it); // Libérer chaque unsigned long int alloué
+    }
 }
 
 PmergeMe::PmergeMe(PmergeMe const &copy) {
@@ -17,21 +19,18 @@ PmergeMe &PmergeMe::operator=(PmergeMe const &copy) {
 }
 
 void PmergeMe::fillVector(char **av) {
-    std::vector<void *> tmp;
+    // std::vector<void *> tmp;
     
     for (size_t i = 1; av[i]; i++) {
-        tmp.push_back(static_cast<void *>(atoui(av[i])));
+        _vec.push_back(static_cast<void *>(atoui(av[i])));
     }
 
     // Afficher les valeurs contenues dans le vecteur
-    for (std::vector<void *>::iterator it = tmp.begin(); it != tmp.end(); ++it) {
+    for (std::vector<void *>::iterator it = _vec.begin(); it != _vec.end(); ++it) {
         unsigned long int *value = static_cast<unsigned long int *>(*it);
         std::cout << *value << " "; // Déréférencer pour obtenir la valeur
     }
     std::cout << std::endl;
 
     // Libérer la mémoire allouée
-    for (std::vector<void *>::iterator it = tmp.begin(); it != tmp.end(); ++it) {
-        delete static_cast<unsigned long int *>(*it); // Libérer chaque unsigned long int alloué
-    }
 }
